794587a6a83a43280b368f4b0ee194cf36fca5a0
Thet Myat Noe Thwin <u6530213@au.edu>
2024-10-27 15:08:59 +0700

    added popular movies

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -29,7 +29,7 @@ const Navbar: React.FC = () => {
         </div>
       </div>
       <div className="navbar-center">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Movies</a>
       </div>
       <div className="navbar-end">
         <button className="btn btn-ghost btn-circle">
@@ -48,18 +48,14 @@ const Navbar: React.FC = () => {
         </button>
         <button className="btn btn-ghost btn-circle">
           <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" 
+            width="2em" 
+            height="2em" 
+            viewBox="0 0 24 24">
+            <path 
+              fill="currentColor" 
+              d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53z"/>
+          </svg>
             <span className="badge badge-xs badge-primary indicator-item"></span>
           </div>
         </button>
